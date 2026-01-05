a9a79c6b33c5cc2eb70d4e461232d6b4cd6d8647
azimut <azimut.github@protonmail.com>
2026-01-05 09:48:59 -0300

    refactor naive

--- seven/naivedigitdb/naivedigitdb.asm
+++ seven/naivedigitdb/naivedigitdb.asm
@@ -10,9 +10,6 @@
 .equ    maskclocknot,   ~maskclock
 .equ    maskdata,       0x80|maskenable
 
-.equ    numberzero,     0B10110111
-.equ    numberone,      0B00100100
-
 
         .org 0x0
 reset:
@@ -23,11 +20,17 @@ reset:
 
         .org 0x10
 main:
-        mov     r0,     #numberzero
+        mov     a,      #4            ;  A = 4
+        add     a,      #segments     ;  A = A + #segments
+        movp    a,      @a            ;  A = @A
+        mov     r0,     a             ; R0 = A
         call    sendnumber
         call    delay
 
-        mov     r0,     #numberone
+        mov     a,      #2            ;  A = 2
+        add     a,      #segments     ;  A = A + #segments
+        movp    a,      @a            ;  A = @A
+        mov     r0,     a             ; R0 = A
         call    sendnumber
         call    delay
 
@@ -70,3 +73,16 @@ delay:
         nop
         nop
         ret
+
+
+segments:
+        .byte #0B10110111         ; 0 0xB7
+        .byte #0B00100100         ; 1 0x24
+        .byte #0B01110011         ; 2 0x73
+        .byte #0B01110110         ; 3 0x76
+        .byte #0B11100100         ; 4 0xE4
+        .byte #0B11010110         ; 5 0xD6
+        .byte #0B11010111         ; 6 0xD7
+        .byte #0B10110100         ; 7 0xB4
+        .byte #0B11110111         ; 8 0xF7
+        .byte #0B11110110         ; 9 0xF6
