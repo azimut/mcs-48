7b65eb907e8534ae82a6e0908b9dd9cbbc214674
azimut <azimut.github@protonmail.com>
2026-01-05 13:51:01 -0300

    rename due filename length issues with dosbox

--- seven/rdigit/rdigit.asm
+++ seven/rdigit/rdigit.asm
@@ -3,12 +3,14 @@
 .area   CODE    (ABS)
 .endif           ; .__.CPU.
 
-
 .equ    maskdisable,    0x00
-.equ    maskenable,     0x20
-.equ    maskclock,      0x40
+.equ    maskenable,     0B10000000
+.equ    maskdata,       0B01000000|maskenable
+.equ    maskclock,      0B00100000
 .equ    maskclocknot,   ~maskclock
-.equ    maskdata,       0x80|maskenable
+
+.equ    numberzero,     0B10110111
+.equ    numberone,      0B00100100
 
 
         .org 0x0
@@ -20,27 +22,21 @@ reset:
 
         .org 0x10
 main:
-        mov     a,      #4            ;  A = 4
-        add     a,      #segments     ;  A = A + #segments
-        movp    a,      @a            ;  A = @A
-        mov     r0,     a             ; R0 = A
-        call    sendnumber
+        mov     r0,     #numberzero
+        call    sendsegment
         call    delay
 
-        mov     a,      #2            ;  A = 2
-        add     a,      #segments     ;  A = A + #segments
-        movp    a,      @a            ;  A = @A
-        mov     r0,     a             ; R0 = A
-        call    sendnumber
+        mov     r0,     #numberone
+        call    sendsegment
         call    delay
 
         jmp     main
 
 
-sendnumber:
+sendsegment:
         mov     a,      #maskenable    ; ENABLE=ON
         outl    p2,     a
-        mov     r5,     #0x08          ; init loop counter
+        mov     r5,     #8             ; init loop counter
 datasend:
         mov     a,      r0
         anl     a,      #0x01
@@ -73,16 +69,3 @@ delay:
         nop
         nop
         ret
-
-
-segments:
-        .byte #0B10110111         ; 0 0xB7
-        .byte #0B00100100         ; 1 0x24
-        .byte #0B01110011         ; 2 0x73
-        .byte #0B01110110         ; 3 0x76
-        .byte #0B11100100         ; 4 0xE4
-        .byte #0B11010110         ; 5 0xD6
-        .byte #0B11010111         ; 6 0xD7
-        .byte #0B10110100         ; 7 0xB4
-        .byte #0B11110111         ; 8 0xF7
-        .byte #0B11110110         ; 9 0xF6
